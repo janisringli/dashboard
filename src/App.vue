75f247d991d9b8a37be0efe0084139e189f46e1d
Janis Ringli <janis.ringli@gmail.com>
2022-05-05 17:01:18 +0200

    fuck you eslint

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <wide-box />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import WideBox from "@/components/WideBox.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    WideBox,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  font-size: 8px;
 }
 </style>
---
+++ src/components/WideBox.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="wide-box__wrapper">Hello World</div>
+</template>
+<style lang="scss" scoped src="@/styles/WideBox.scss"></style>
+<script>
+export default {
+  name: "WideBox",
+};
+</script>

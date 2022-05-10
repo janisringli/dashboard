3bdd28b34b1c502b6bef692e0615006ba4efdd8b
Janis Ringli <janis.ringli@gmail.com>
2022-05-10 23:38:07 +0200

    deleted useless code

--- src/components/WideBox.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="wide-box__wrapper"><slot></slot></div>
-</template>
-<style lang="scss" scoped src="@/styles/WideBox.scss"></style>
-<script>
-export default {
-  name: "WideBox",
-};
-</script>

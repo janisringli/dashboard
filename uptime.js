846d4c513d75051108eb3ff5ee123825cc88801f
“Janis <janis.ringli@gmail.com>
2022-05-27 22:00:30 +0200

    add new website to uptime

--- uptime.js
+++ uptime.js
@@ -1,14 +1,12 @@
 function portfolioRequest() {
   fetch("https://ja.nisringli.ch", { mode: "no-cors" })
     .then((response) => {
-      console.log(response.status);
       if (response.status == 0) {
         document.getElementById("uptimeLightPortfolio").style.background =
           "#8DEA97";
       }
     })
     .catch((err) => {
-      console.log(err);
       document.getElementById("uptimeLight").style.background = "#EB5151";
     });
 }
@@ -16,34 +14,43 @@ function portfolioRequest() {
 function youtubeRequest() {
   fetch("https://youtube.com", { mode: "no-cors" })
     .then((response) => {
-      console.log(response.status);
       if (response.status == 0) {
         document.getElementById("uptimeLightYoutube").style.background =
           "#8DEA97";
       }
     })
     .catch((err) => {
-      console.log(err);
       document.getElementById("uptimeLight").style.background = "#EB5151";
     });
 }
 function trailyRequest() {
   fetch("https://traily.ch", { mode: "no-cors" })
     .then((response) => {
-      console.log(response.status);
       if (response.status == 0) {
         document.getElementById("uptimeLightTraily").style.background =
           "#8DEA97";
       }
     })
     .catch((err) => {
-      console.log(err);
       document.getElementById("uptimeLight").style.background = "#EB5151";
     });
+}
+    function calcRequest() {
+      fetch("https://calc.nisringli.ch", { mode: "no-cors" })
+        .then((response) => {
+          if (response.status == 0) {
+            document.getElementById("uptimeLightCalc").style.background =
+              "#8DEA97";
+          }
+        })
+        .catch((err) => {
+          document.getElementById("uptimeLightCalc").style.background = "#EB5151";
+        });
 }
 youtubeRequest();
 portfolioRequest();
 trailyRequest();
+calcRequest()
 setInterval(function () {
   youtubeRequest();
   portfolioRequest();
